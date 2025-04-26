86f3b4357295501b116190b9b72d7ed8888fdeba
Michael Luo <mluo.lion@gmail.com>
2025-04-25 20:48:00 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 (async function () {
   const projects = await fetchJSON('../lib/projects.json');
+  console.log(projects);
   const projectsContainer = document.querySelector('.projects');
   renderProjects(projects, projectsContainer, 'h2');
 })();
