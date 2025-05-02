07de7a01ec0ce49b9ee012c12918863c47ebb3a0
Michael Luo <mluo.lion@gmail.com>
2025-05-02 00:09:11 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -13,11 +13,11 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let data = [1, 2]; 
+let data = [1, 2, 3, 4, 5, 5]; 
 let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
-let colors = ['gold', 'purple'];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 arcs.forEach((arc, idx) => {
   d3.select('#projects-pie-plot')
     .append('path')
