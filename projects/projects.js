10bdd55656f893e701e43da07d87e05d8fb44b20
Michael Luo <mluo.lion@gmail.com>
2025-05-02 00:17:34 -0700

    x

--- projects/projects.js
+++ projects/projects.js
@@ -10,11 +10,18 @@ import { fetchJSON, renderProjects } from '../global.js';
     projectsTitle.textContent = `Projects: ${projects.length}`;
   }
 })();
-
-
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let data = [1, 2, 3, 4, 5, 5]; 
-let sliceGenerator = d3.pie();
+
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' }
+];
+
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
@@ -22,5 +29,20 @@ arcs.forEach((arc, idx) => {
   d3.select('#projects-pie-plot')
     .append('path')
     .attr('d', arc)
-    .attr('fill', colors[idx]);
+    .attr('fill', colors(idx));
+});
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('text')
+    .attr('transform', `translate(${arcGenerator.centroid(arc)})`)
+    .attr('text-anchor', 'middle')
+    .attr('dy', '.35em')
+    .text(data[idx].label); 
+});
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`)  
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);  
 });
