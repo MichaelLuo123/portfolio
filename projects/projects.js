6e09269030eaae8b67e2e690969052500860bcdf
Michael Luo <mluo.lion@gmail.com>
2025-05-03 01:37:08 -0700

    x

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,11 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
+let query = '';
+let projects = [];
+
 (async function () {
-  const projects = await fetchJSON('../lib/projects.json');
+  projects = await fetchJSON('../lib/projects.json');
   const projectsContainer = document.querySelector('.projects');
   renderProjects(projects, projectsContainer, 'h2');
   const projectsTitle = document.querySelector('.projects-title');
@@ -41,3 +44,21 @@ data.forEach((d, idx) => {
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
 })();
+const searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener('input', (event) => {
+  query = event.target.value.toLowerCase();
+
+  const filteredProjects = projects.filter((project) => {
+    const values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query);
+  });
+
+  const projectsContainer = document.querySelector('.projects');
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+
+  const projectsTitle = document.querySelector('.projects-title');
+  if (projectsTitle) {
+    projectsTitle.textContent = `Projects: ${filteredProjects.length}`;
+  }
+});
